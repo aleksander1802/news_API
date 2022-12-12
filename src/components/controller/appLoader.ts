97b3c9f85bcc63775482fcceea40234d0b8cbd26
Alex2 <aleksander1802@gmail.com>
2022-12-12 17:22:34 +0300

    feat: migrate appLoader.js to appLoader.ts

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,6 +3,9 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
+            // https://newsapi.org/v2/
+            // https://nodenews.up.railway.app/
+            // https://newsapi-redirect-production.up.railway.app/
             apiKey: '99ad52376bb54747aa23434e51513238', // получите свой ключ https://newsapi.org/
         });
     }
