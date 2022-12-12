9bd488fda11ce9f9c7fd19db6ca7c968a8069c70
Alex2 <aleksander1802@gmail.com>
2022-12-12 17:35:10 +0300

    feat: migrate app.js to app.ts

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,16 +1,21 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { typeSafe } from '../view/news/news';
 
 class App {
+    controller: AppController;
+    view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
     start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+        typeSafe(document, '.sources').addEventListener('click', (e) =>
+            this.controller.getNews(e, (data) => this.view.drawNews(data))
+        );
+
         this.controller.getSources((data) => this.view.drawSources(data));
     }
 }
