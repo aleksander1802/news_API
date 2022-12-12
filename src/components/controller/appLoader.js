d00b1167bb32ebd4f1aca89afae4a795eb61ee80
Alex2 <aleksander1802@gmail.com>
2022-12-12 16:23:30 +0300

    feat: configure development environment, set up apiKey

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '99ad52376bb54747aa23434e51513238', // получите свой ключ https://newsapi.org/
         });
     }
 }
