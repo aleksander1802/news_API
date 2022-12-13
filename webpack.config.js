93ffd487c98c0fd5eac70cb65b3f950ea839a1b2
Alex2 <aleksander1802@gmail.com>
2022-12-13 21:50:19 +0300

    feat: add properties to global.css, change webpack.config.js, rebuild project

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslingPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, 'src', 'index.ts'),
@@ -37,7 +38,13 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
-        new EslingPlugin({ extensions: 'ts' })
+        new EslingPlugin({ extensions: 'ts' }),
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, 'src/assets'),
+                to: path.resolve(__dirname, 'dist/assets')
+            }]
+        })
     ],
 };
 
