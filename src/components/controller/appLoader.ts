aa0e7ad55cbeafdaed1ce997d7b8365cb8b0f4ed
Alex2 <aleksander1802@gmail.com>
2022-12-14 15:12:03 +0300

    feat: change properties at sources.css

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -6,7 +6,6 @@ class AppLoader extends Loader {
             // https://newsapi.org/v2/
             // https://nodenews.up.railway.app/
             // https://newsapi-redirect-production.up.railway.app/
-            // без понятия в чем ошибка, npm start работает без проблем, при деплое ссылки зеркал не работают
             apiKey: '99ad52376bb54747aa23434e51513238', // получите свой ключ https://newsapi.org/
         });
     }
